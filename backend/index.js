d493e76fb81a75eea1a0206c28239b3bf962ced0
Rubens David Alvares da Silva <alvares.rubens@gmail.com>
2020-03-23 18:18:24 -0300

    Change app list structure to heroku

--- backend/index.js
+++ backend/index.js
@@ -86,6 +86,4 @@ const checkPrime = (divisorList) => {
     return false;
 }
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
-});
+app.listen(process.env.PORT || 5000);
